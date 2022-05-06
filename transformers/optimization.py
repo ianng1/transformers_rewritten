0826165e8bc76722e8043bad825adf89aedbde9b
Peng Wah Ng <pengwah@DN51vt9k.SUNet>
2022-05-06 02:45:50 -0700

    sparse gradient

--- transformers/optimization.py
+++ transformers/optimization.py
@@ -327,15 +327,73 @@ class AdamW(Optimizer):
         Arguments:
             closure (`Callable`, *optional*): A closure that reevaluates the model and returns the loss.
         """
-        
+        """
         loss = None
         if closure is not None:
             loss = closure()
 
         for group in self.param_groups:
             for p in group["params"]:
-                state = self.state['p']
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
+
+                state["step"] += 1
+
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
+                exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
+
+                step_size = group["lr"]
+                if group["correct_bias"]:  # No bias correction for Bert
+                    bias_correction1 = 1.0 - beta1 ** state["step"]
+                    bias_correction2 = 1.0 - beta2 ** state["step"]
+                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+
+                p.data.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # Just adding the square of the weights to the loss function is *not*
+                # the correct way of using L2 regularization/weight decay with Adam,
+                # since that will interact with the m and v parameters in strange ways.
+                #
+                # Instead we want to decay the weights in a manner that doesn't interact
+                # with the m/v parameters. This is equivalent to adding the square
+                # of the weights to the loss with plain (non-momentum) SGD.
+                # Add weight decay at the end (fixed version)
+                if group["weight_decay"] > 0.0:
+                    p.data.add_(p.data, alpha=(-group["lr"] * group["weight_decay"]))
+
+        return loss
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+        print("\nRunning ADAM optimizer")
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
                 gradient = p.grad.data
+                if gradient.is_sparse:
+                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+                state = self.state['p']
                 if (len(self.state['p'].keys()) == 0):
                     state['timestep'] = 1
                     state['m'] = torch.zeros(p.data.shape)
@@ -356,29 +414,8 @@ class AdamW(Optimizer):
                 state['v'] = v
                 state['timestep'] += 1
                 p.data += correction
-                # Just adding the square of the weights to the loss function is *not*
-                # the correct way of using L2 regularization/weight decay with Adam,
-                # since that will interact with the m and v parameters in strange ways.
-                #
-                # Instead we want to decay the weights in a manner that doesn't interact
-                # with the m/v parameters. This is equivalent to adding the square
-                # of the weights to the loss with plain (non-momentum) SGD.
-                # Add weight decay at the end (fixed version)
-                if group["weight_decay"] > 0.0:
-                    p.data.add_(p.data, alpha=(-group["lr"] * group["weight_decay"]))
-
-        return loss
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-        print("\nRunning ADAM optimizer")
-        for group in self.param_groups:
-            for p in group['params']:
-
                 
         return loss
-        """
 
 
                 
